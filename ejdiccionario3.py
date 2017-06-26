f65635807a683c7e60dd7d16cbaa7cf31ccf94f9
Udem <udem@fedora.mshome.net>
2017-06-26 17:59:25 -0600

    ejercicio 3

---
+++ ejdiccionario3.py
@@ -0,0 +1,23 @@
+import random
+
+NOMBRES = [ 'Ana','Pedro','Pablo', 'Ernesto','Ariel','Carlos','Luis','Oscar', 'Alicia','Maria','Brenda']
+
+CIUDADES = ['Managua','Masaya','Matagalpa', 'Chinandega','Somoto', 'Rivas']
+
+
+def generar_diccionario_estudiantes():
+    estudiantes = {}
+
+    for nombre in NOMBRES:
+        estudiantes = {
+
+            'Nombre': random.choice(NOMBRES),
+            'edad': random.randrange(16, 30),
+            'anio': random.randrange(1, 5),
+            'cuidad': random.choice(CIUDADES)
+
+        }
+
+    return estudiantes
+
+print = generar_diccionario_estudiantes()
